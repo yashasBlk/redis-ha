dd721ba283094a17ec0a3058663c0c67d859917d
yashasBlk <52620498+yashasBlk@users.noreply.github.com>
2019-11-07 21:04:23 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -132,14 +132,13 @@ function launchslave() {
     echo "Connecting to master failed.  Waiting..."
     sleep 10
   done
-  
+  mkdir -p ${REDIS_DATADIR}/${REDIS_POD_NAME:-noname}
+  sed -i "s/%master-ip%/${master}/" ${HOME}/redis-slave/redis.conf
+  sed -i "s/%master-port%/6379/" ${HOME}/redis-slave/redis.conf
+  sed -i "s/%slave-data%/${REDIS_POD_NAME:-slave}/" ${HOME}/redis-slave/redis.conf
+  ${REDIS_PREFIX}/bin/redis-server ${HOME}/redis-slave/redis.conf
 
   
-  mkdir -p ${REDIS_DATADIR}/${REDIS_POD_NAME:-noname}
-  sed -i "s/%master-ip%/${master}/" /etc/redis/redis.conf
-  sed -i "s/%master-port%/6379/" /etc/redis/redis.conf
-  sed -i "s/%slave-data%/${REDIS_POD_NAME:-slave}/" /etc/redis/redis.conf
-  ${REDIS_PREFIX}/bin/redis-server /etc/redis/redis.conf
 }
 
 if [[ "${MASTER:-false}" == "true" ]]; then
