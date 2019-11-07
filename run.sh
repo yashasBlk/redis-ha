5a1e1a0ce0c4d8ef0406fa7b3dbc1f1ad011f6a1
yashasBlk <52620498+yashasBlk@users.noreply.github.com>
2019-11-07 17:43:06 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -77,7 +77,7 @@ function launchmaster() {
 
   restore
 
-  ${REDIS_PREFIX}/bin/redis-server /etc/redis/:redis.conf
+  ${REDIS_PREFIX}/bin/redis-server /etc/redis/redis.conf
 }
 
 function launchsentinel() {
